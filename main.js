e173fcb1bab521fe908b0596c9d5b599c8923102
Marino <marinobtt@gmail.com>
2022-03-10 13:42:02 -0300

    Confirmado

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function tocaSomPom() {
-    document.querySelector("#som_tecla_pom").play()
+function tocaSom(idAudio) { 
+    document.querySelector(idAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla'); //cria uma lista de elementos referente a classe tecla
 
-let i = 0
-while (i < listaDeTeclas.length) {
-    listaDeTeclas[i].onclick = tocaSomPom;
-    i++
+
+for(let c=0; c < listaDeTeclas.length; c++) {
+    const tecla = listaDeTeclas[c]; //atribui cada elemento da lista e atribui para a constante
+    const classeTecla = tecla.classList[1]; //atribui o segundo elemento da propriedade classList (class do elemento)
+    const idAudio = `#som_${classeTecla}` //template string. Cria uma string dinamicamente, para isso deve estar entre `crazes` e a variável entre {colchetes}
+
+    tecla.onclick = function () { //ao clicar na tecla chama a função anônima
+        tocaSom(idAudio) //chama a função com um parâmetro da id do <audio>
+    };
 }
